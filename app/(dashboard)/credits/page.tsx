7b6a38d4234b57b89fda90a003d0a9a3f3402bef
YourDevVince <vince.dev.help@gmail.com>
2026-02-13 08:04:21 -0600

    feat: credit options added as component and credit route connected

--- app/(dashboard)/credits/page.tsx
+++ app/(dashboard)/credits/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import './credits.css';
+import CreditOpts from '@/app/_components/CreditOpts/CreditOpts';
 function page() {
   return (
     <div className='credits'>
@@ -7,23 +7,24 @@ function page() {
         <h1 className='credits__header-title'>Credits</h1>
         <p className='credits__header-subtext'>
           You have 3 credits available.<br></br> 1 credit equals an hour of
-          tutoring for your child
+          tutoring for your child.
         </p>
       </header>
-      <div className='billing'>
+      <section className='billing'>
         <h2 className='billing__title'>Billing information</h2>
         <div className='billing__card'>
           <div className='billing__card-info'>
             <p>
-              Card nickname <span>turtle debit</span>
+              Card nickname: <span>turtle debit</span>
             </p>
             <p>
-              Ending in <span>xxxx-xxxx-xxxx-5432</span>
+              Ending in: <span>xxxx-xxxx-xxxx-5432</span>
             </p>
           </div>
           <div className='billing__card-btn'>Edit</div>
         </div>
-      </div>
+      </section>
+      <CreditOpts />
     </div>
   );
 }
