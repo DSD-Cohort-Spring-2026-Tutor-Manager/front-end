71aec6decf01b5a4eeda8ce72efb854f343e07bf
YourDevVince <vince.dev.help@gmail.com>
2026-02-27 03:20:34 -0600

    feat: gave data boxes the option to have a subtitle for admin

--- app/_components/DataBox/Databox.tsx
+++ app/_components/DataBox/Databox.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import "./Databox.css";
+import Link from 'next/link';
+import './Databox.css';
 
 type TopRightIcon = {
   src: string;
@@ -9,6 +9,7 @@ type TopRightIcon = {
 
 function Databox({
   title,
+  subtitle,
   value,
   cta,
   href,
@@ -18,6 +19,7 @@ function Databox({
   dropdownOnChange,
 }: {
   title: string;
+  subtitle?: string;
   value: string | number;
   cta?: string;
   href?: string;
@@ -27,14 +29,16 @@ function Databox({
   dropdownOnChange?: (selectedOption: any) => void;
 }) {
   return (
-    <div className="databox-sm w-full h-80 bg-(--Primary) rounded-xl relative">
-      <h2 className="databox__title">{title}</h2>
-      <p className="databox__title-value">{value}</p>
+    <div className='databox-sm w-full h-80 bg-(--Primary) rounded-xl relative'>
+      <h2 className='databox__title'>{title}</h2>
+
+      <p className='databox__title-value'>{value}</p>
+      {subtitle ? <h3 className='databox__subtitle'>{subtitle}</h3> : ''}
       {cta &&
         (dropdownContent?.length ? (
-          <div className="dropdown databox_btn">
-            <button className="dropbtn">Switch</button>
-            <div className="dropdown-content">
+          <div className='dropdown databox_btn'>
+            <button className='dropbtn'>Switch</button>
+            <div className='dropdown-content'>
               {dropdownContent.map((e, index) => (
                 <a
                   key={`option-${index}`}
@@ -46,14 +50,14 @@ function Databox({
             </div>
           </div>
         ) : (
-          <Link className="databox__btn" href={href ?? "#"} onClick={onClick}>
+          <Link className='databox__btn' href={href ?? '#'} onClick={onClick}>
             {cta}
           </Link>
         ))}
 
       {topRightIcon && (
         <img
-          className="databox__tr-image"
+          className='databox__tr-image'
           src={topRightIcon.src}
           alt={topRightIcon.alt}
           onClick={() => topRightIcon.onClick?.()}
--- app/parent/page.tsx
+++ app/parent/page.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { useContext, useEffect, useState } from "react";
-import { CreditContext } from "@/app/_components/CreditContext/CreditContext";
-import Databox from "../_components/DataBox/Databox";
-import DataboxMed from "../_components/DataBox/DataboxMed";
-import CreditsViewBar from "../_components/CreditsViewbar/CreditsViewBar";
-import "./dashboard.css";
-import { TutortoiseClient } from "../_api/tutortoiseClient";
-import Modal from "../_components/Modal/Modal";
-import Alert from "../_components/Alert/Alert";
-import { StudentContext } from "../context/StudentContext";
+'use client';
+import { useContext, useEffect, useState } from 'react';
+import { CreditContext } from '@/app/_components/CreditContext/CreditContext';
+import Databox from '../_components/DataBox/Databox';
+import DataboxMed from '../_components/DataBox/DataboxMed';
+import CreditsViewBar from '../_components/CreditsViewbar/CreditsViewBar';
+import './dashboard.css';
+import { TutortoiseClient } from '../_api/tutortoiseClient';
+import Modal from '../_components/Modal/Modal';
+import Alert from '../_components/Alert/Alert';
+import { StudentContext } from '../context/StudentContext';
 
 type Student = {
   studentId: number;
@@ -45,21 +45,21 @@ function Home() {
 
   const ctx = useContext(CreditContext);
   if (!ctx)
-    throw new Error("CreditContext is missing. Wrap app in CreditProvider.");
+    throw new Error('CreditContext is missing. Wrap app in CreditProvider.');
 
   const { credits, addCredits } = ctx;
 
   const studentCtx = useContext(StudentContext);
   if (!studentCtx)
     throw new Error(
-      "StudentContext is missing. Wrap the app in StudentProvider.",
+      'StudentContext is missing. Wrap the app in StudentProvider.',
     );
 
   const { student, setStudent } = studentCtx;
 
   function getCompletedSessions(sessions: Session[], studentId: number) {
     return sessions.filter(
-      (s) => s.sessionStatus === "completed" && s.studentId === studentId,
+      (s) => s.sessionStatus === 'completed' && s.studentId === studentId,
     );
   }
 
@@ -74,8 +74,8 @@ function Home() {
   }
 
   function addStudent() {
-    const firstName = (document.getElementById("firstName") as any)?.value;
-    const lastName = (document.getElementById("lastName") as any)?.value;
+    const firstName = (document.getElementById('firstName') as any)?.value;
+    const lastName = (document.getElementById('lastName') as any)?.value;
 
     const showSuccessAlert = () => {
       setIsAlertExiting(true);
@@ -107,7 +107,7 @@ function Home() {
     TutortoiseClient.getParentDetails(1).then((res) => {
       console.log(res);
     });
-    TutortoiseClient.getBalance("1").then((res: number) => {
+    TutortoiseClient.getBalance('1').then((res: number) => {
       addCredits(-credits + res);
     });
   }, []);
@@ -132,7 +132,7 @@ function Home() {
         setCompletedSess(completedSessions);
         setLatesTwo(latest);
       } catch (err) {
-        console.error("Failed to load the sessions:", err);
+        console.error('Failed to load the sessions:', err);
       }
     };
     loadSessions();
@@ -159,58 +159,58 @@ function Home() {
   }, [isAlertVisible]);
 
   return (
-    <main className="dashboard overflow-x-hidden h-full">
+    <main className='dashboard overflow-x-hidden h-full'>
       <CreditsViewBar
         value={credits.toString()}
-        href="/parent/credits"
-        cta="Need more credits?"
+        href='/parent/credits'
+        cta='Need more credits?'
       />
-      <section className="dashboard__data-row">
+      <section className='dashboard__data-row'>
         <Databox
-          title="Student"
-          value={student?.studentName?.split(" ")[0]}
-          href="/student"
-          cta="switch"
+          title='Student'
+          value={student?.studentName?.split(' ')[0]}
+          href='/student'
+          cta='switch'
           topRightIcon={{
-            src: "/icons/Add user icon.svg",
-            alt: "Add student button",
+            src: '/icons/Add user icon.svg',
+            alt: 'Add student button',
             onClick: () => setAddStudentModalIsOpen(true),
           }}
-          dropdownContent={[{ label: "Zayn" }, { label: "Student2" }]}
+          dropdownContent={[{ label: 'Zayn' }, { label: 'Robert' }]}
           dropdownOnChange={setStudent}
         />
         <Databox
-          title="Sessions completed"
+          title='Sessions completed'
           value={completedSess.length.toString()}
-          href="/student"
-          cta="View"
+          href='/student'
+          cta='View'
         />
         <DataboxMed latest={latestTwo} />
         {isAddStudentModalOpen && (
           <Modal
-            type="add student"
-            text=""
+            type='add student'
+            text=''
             buttons={[
               {
-                className: "add-student-confirm-button",
-                text: "Add Student",
+                className: 'add-student-confirm-button',
+                text: 'Add Student',
                 onClick: () => addStudent(),
               },
               {
-                className: "add-student-cancel-button",
-                text: "Cancel",
+                className: 'add-student-cancel-button',
+                text: 'Cancel',
                 onClick: () => setAddStudentModalIsOpen(false),
               },
             ]}
           />
         )}
       </section>
-      <div className="alert-layer">
+      <div className='alert-layer'>
         {isAlertVisible && (
           <Alert
-            type="success"
-            text="Student Created!"
-            className={isAlertExiting ? "alert-exit" : "alert-enter"}
+            type='success'
+            text='Student Created!'
+            className={isAlertExiting ? 'alert-exit' : 'alert-enter'}
           />
         )}
       </div>
